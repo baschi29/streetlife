bee766c7d0126ea56029317707accc7723765e8b
Cornelius Brütt <Cornelius@DESKTOP-ALK9CRH.fritz.box>
2021-12-11 23:33:16 +0100

    added some life to the street

--- streetlife/src/infpp/streetlife/StreetLifeMain.java
+++ streetlife/src/infpp/streetlife/StreetLifeMain.java
@@ -15,10 +15,21 @@ public class StreetLifeMain {
 	 */
 	public static void main(String[] args) {
 		
+		
 		int width = 1024;
 		int length = 768;
-		Car car = new Car(width/2, 0, "Flotter Fiat", 5);
-		Street city = new Street(width, depth, car);
+		Car car1 = new Car(width/2, 0, "Flotter Fiat", 5);
+		Car car2 = new Car(width/2, 0, "Fantastischer Ford", 6);
+		Car car3 = new Car(width/2, 0, "angenehmer Audi", 4);
+		Car car4 = new Car(width/2, 0, "cooles Cabrio", 3);
+		Car car5 = new Car(width/2, 0, "bombastischer BMW", 9);
+		Street city = new Street(width, length);
+		
+		city.addCar(car1);
+		city.addCar(car2);
+		city.addCar(car3);
+		city.addCar(car4);
+		city.addCar(car5);
 		
 		for (int i = 0; i < 5; i++) {
 			city.move();
