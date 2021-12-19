74ad8161bd2ccc45058ff6541e5e9e19e7231f0d
Cornelius Brütt <cornelius,bruett@gmx.de>
2021-12-19 21:32:33 +0100

    Added a functioning controller with a set amount of movement ticks

--- streetlife/src/infpp/streetlife/controller/StreetLifeController.java
+++ streetlife/src/infpp/streetlife/controller/StreetLifeController.java
@@ -3,9 +3,41 @@
  */
 package infpp.streetlife.controller;
 
+import infpp.streetlife.model.StreetLifeModel;
+import infpp.streetlife.view.View;
+
 /**
  *
  */
-public class StreetLifeController {
+public class StreetLifeController implements Controller {
 
+	private int numberTicks;
+	private StreetLifeModel model;
+	private View view;
+	
+	
+	/**
+	 * Creates a new Controller
+	 * @param numTicks specifies the amount of ticks
+	 */
+	public StreetLifeController(int numTicks, StreetLifeModel model){
+		this.numberTicks = numTicks;
+		this.model = model;
+		
+	}
+
+	@Override
+	public void start() {
+		
+		for(int n = 0; n < numberTicks; n ++) {
+			model.move();
+			model.getView().display();
+		}
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
 }
