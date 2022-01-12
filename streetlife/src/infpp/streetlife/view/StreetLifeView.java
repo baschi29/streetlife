7a129ac3bfde4617865659e61374e05fe52f6d9e
Cornelius Brütt <cornelius.bruett@gmx.de>
2022-01-12 14:27:59 +0100

    added first ComboBox-function. Needs some serious work tho!

--- streetlife/src/infpp/streetlife/view/StreetLifeView.java
+++ streetlife/src/infpp/streetlife/view/StreetLifeView.java
@@ -9,12 +9,12 @@ import infpp.streetlife.model.StreetObject;
 
 public class StreetLifeView implements View {
 
-	private StreetLifeGUI gui = new StreetLifeGUI();
+	private StreetGUI gui;
 	
 	@Override
-	public void build(int numberLanes, int length, ArrayList<StreetObject> modelState) {
+	public void build(int numberLanes, int length, ArrayList<StreetObject> modelState) { //ist ne initiale Größe überhaupt noch notwendig? Würde bei 0 anfangen
 		
-		
+		gui = new StreetGUI(modelState);
 		gui.setVisible(true);
 		
 
