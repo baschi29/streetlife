e2584322c6da4234bf64cc7a6ca7ba9cb771b62f
baschi29 <baschi29@yahoo.de>
2022-01-31 10:48:07 +0100

    Better resource structure, started usin getResouceAsStream() for all Resources, needs work

--- streetlife/src/infpp/streetlife/view/StartUpDialog.java
+++ streetlife/src/infpp/streetlife/view/StartUpDialog.java
@@ -10,8 +10,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -47,7 +49,7 @@ public class StartUpDialog  extends JDialog implements ActionListener {
 	private final String WELCOME_PATH = "txt/welcome.txt";
 
 	//path to the used image
-	private final String FROG_PATH = "/frog.png";
+	private final String FROG_PATH = "img/frog.png";
 	private JTextField textFieldLength;
 	private JTextField textFieldLanes;
 	private JTextField textFieldPath;
@@ -82,16 +84,21 @@ public class StartUpDialog  extends JDialog implements ActionListener {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
-		
-		ImageIcon img = new ImageIcon(this.getClass().getResource(FROG_PATH));
-		this.setIconImage(img.getImage());
-		{
-			JLabel imgpanel = new JLabel();
-			imgpanel.setIcon(new ImageIcon(img.getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
-			contentPanel.add(imgpanel, BorderLayout.WEST);
+		try {
+			ImageIcon img = new ImageIcon(ImageIO.read(this.getClass().getClassLoader().getResourceAsStream(FROG_PATH)));
+			this.setIconImage(img.getImage());
 			
-			
+			{
+				JLabel imgpanel = new JLabel();
+				imgpanel.setIcon(new ImageIcon(img.getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
+				contentPanel.add(imgpanel, BorderLayout.WEST);
+				
+			}
 		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		{
 			JPanel panel = new JPanel();
 			contentPanel.add(panel, BorderLayout.CENTER);
@@ -302,16 +309,17 @@ public class StartUpDialog  extends JDialog implements ActionListener {
 	}
 		
 	private String loadFile(String filepath) {
+		InputStream in;
 		Scanner sc;
 		String str = "";
 		try {
-			
-			sc = new Scanner(new File(filepath));
+			in = this.getClass().getClassLoader().getResourceAsStream(filepath);
+			sc = new Scanner(in);
 			while(sc.hasNextLine()){
 			    str += sc.nextLine();                     
 			}
 			
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			System.out.println("Error: File not found!");
 			e.printStackTrace();
 		}
