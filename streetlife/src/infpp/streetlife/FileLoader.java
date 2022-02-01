742d32b200f092255a83b0d9962dc98cb83a397b
Cornelius Brütt <cornelius.bruett@gmx.de>
2022-02-01 15:11:46 +0100

    updated FileLoader to implement text loading

--- streetlife/src/infpp/streetlife/FileLoader.java
+++ streetlife/src/infpp/streetlife/FileLoader.java
@@ -1,16 +1,23 @@
 package infpp.streetlife;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
-import java.awt.image.BufferedImage;
 /**
  * The FileLoader does exactly what the name suggest: loading files. Used everywhere as a utility class to load Image/ImageIcons from resourcepaths. Uses Resourcestreams to ensure nice and smooth .jar-packing
  * @author Cornelius
+ * @version 1.3
+ * @since 2022-02-01
  *
  */
+
+
+
 public class FileLoader {
 
 	/**
@@ -20,8 +27,12 @@ public class FileLoader {
 	 * @throws IOException in case of path not found
 	 */
 	public ImageIcon loadImageIcon(String filepath) throws IOException{
-		ImageIcon img = new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream(filepath)));
-		return img;
+		try {
+			ImageIcon img = new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream(filepath)));
+			return img;
+		} catch (IOException e) {
+			throw new IOException("Error during Image Loading");
+		}
 	
 	}
 	
@@ -32,8 +43,38 @@ public class FileLoader {
 	 * @throws IOException in case of path not found
 	 */
 	public BufferedImage loadImage(String filepath) throws IOException{
-		BufferedImage img = ImageIO.read(getClass().getClassLoader().getResourceAsStream(filepath));
-		return img;
+		try {
+			BufferedImage img = ImageIO.read(getClass().getClassLoader().getResourceAsStream(filepath));
+			return img;
+		} catch (IOException e) {
+			throw new IOException("Error during Image Loading");
+		}
 	
 	}
+	
+	/**
+	 * Loads a file and glues every line onto a long String
+	 * @param filepath location if file
+	 * @return content of file in String
+	 * @throws IOException
+	 */
+	public String loadFileAsString(String filepath) throws IOException{
+		Scanner sc;
+		String str = "";
+		
+		try {
+			sc = new Scanner(new File(filepath));
+			
+			while(sc.hasNextLine()){
+				str += sc.nextLine();     
+			}
+			return str;
+		} catch (FileNotFoundException e) {
+			throw new IOException("Error during File Loading");
+		}
+	}
+		
+	
+	
 }
+
