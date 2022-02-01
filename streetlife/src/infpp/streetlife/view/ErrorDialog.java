353a5eda5407453da0266c6d290b952e14c7da87
Cornelius Brütt <cornelius.bruett@gmx.de>
2022-02-01 15:12:14 +0100

    fixed bug where TrayIcon stayed after disposing

--- streetlife/src/infpp/streetlife/view/ErrorDialog.java
+++ streetlife/src/infpp/streetlife/view/ErrorDialog.java
@@ -3,8 +3,14 @@ package infpp.streetlife.view;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -15,8 +21,7 @@ import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-import java.awt.*;
-import java.awt.TrayIcon.MessageType;
+import infpp.streetlife.FileLoader;
 /**
  * The ErrorDialog is a custom PopUp window that notifies the user after encountering an error. Includes a system-notification for extra attention.
  * @author Cornelius
@@ -30,15 +35,21 @@ public class ErrorDialog extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 
 	//path to the used image
-	private final String FROG_PATH = "/frog_dead.png";
+	private final String FROG_PATH = "img/frog_dead.png";
+	
+	private FileLoader fl = new FileLoader();
 
+	private SystemTray tray;
+	private TrayIcon trayIcon;
+	
+	
 	/**
 	 * Launch the application. Used for debug
 	 */
 	public static void main(String[] args) {
 		try {
 			ErrorDialog dialog = new ErrorDialog(new Exception("Do not worry! This is an example error message, nothing actually went wrong. If you read this as a normal user, please ignore it"));
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			//dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -59,8 +70,22 @@ public class ErrorDialog extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		
-		ImageIcon img = new ImageIcon(this.getClass().getResource(FROG_PATH));
-		this.setIconImage(img.getImage());
+		ImageIcon img;
+		try {
+			img = fl.loadImageIcon(FROG_PATH);
+			this.setIconImage(img.getImage());
+			
+			{
+				JLabel imgpanel = new JLabel();
+				imgpanel.setIcon(new ImageIcon(img.getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
+				contentPanel.add(imgpanel, BorderLayout.WEST);
+				
+				
+			}
+			
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		
 		JTextArea txtrD = new JTextArea(5, 20);
 		txtrD.setText(exc.getLocalizedMessage());
@@ -68,13 +93,7 @@ public class ErrorDialog extends JDialog {
 		txtrD.setWrapStyleWord(true);
 		txtrD.setEditable(false);
 		contentPanel.add(txtrD, BorderLayout.CENTER);
-		{
-			JLabel imgpanel = new JLabel();
-			imgpanel.setIcon(new ImageIcon(img.getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
-			contentPanel.add(imgpanel, BorderLayout.WEST);
-			
-			
-		}
+		
 		{
 			JPanel buttonPane = new JPanel();
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
@@ -85,7 +104,7 @@ public class ErrorDialog extends JDialog {
 				JButton okButton = new JButton("OK");
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						dispose();
+						closeProgram();
 					}
 				});
 				okButton.setHorizontalAlignment(SwingConstants.LEFT);
@@ -93,6 +112,15 @@ public class ErrorDialog extends JDialog {
 				getRootPane().setDefaultButton(okButton);
 			}
 		}
+		
+		//dont just dispose when closing, but remove the systemTrayIcon
+		this.addWindowListener(new WindowAdapter()
+		{
+		    public void windowClosing(WindowEvent e)
+		    {
+		        closeProgram();
+		    }
+		});
 	}
 	
 	
@@ -103,10 +131,10 @@ public class ErrorDialog extends JDialog {
 		
 		try {
 			//Obtain only one instance of the SystemTray object
-			SystemTray tray = SystemTray.getSystemTray();
-			Image image = Toolkit.getDefaultToolkit().createImage("/frog.png");
+			tray = SystemTray.getSystemTray();
+			Image image = fl.loadImage(FROG_PATH); //Toolkit.getDefaultToolkit().createImage("/frog.png");
 
-			TrayIcon trayIcon = new TrayIcon(image, "Froschsimulator" );
+		    trayIcon = new TrayIcon(image, "Froschsimulator" );
 			//Let the system resize the image if needed
 			trayIcon.setImageAutoSize(true);
 			//Set tooltip text for the tray icon
@@ -119,6 +147,15 @@ public class ErrorDialog extends JDialog {
 			catch(Exception exc){
 				exc.printStackTrace();
 		}
+	}
+	
+	/**
+	 * used for removing the SystemTrayIcon before disposing, so nothing gets left behind
+	 */
+	private void closeProgram() {
+		this.tray.remove(trayIcon);
+		this.dispose();
+		
 	}
 }
 
